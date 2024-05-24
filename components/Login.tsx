6b706dd8026000c0cdde82a7a499d0a648058214
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-24 16:03:52 +0700

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,26 +1,49 @@
 "use client";
 
 import { userLogin } from "@/lib/action";
-import { Button, Card, Container, Flex, TextField } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  TextField,
+} from "@radix-ui/themes";
 
 export function Login() {
   return (
-    <Flex align={"center"} justify={"center"} height={"100dvh"}>
-      <Container size={"1"}>
-        <Card asChild size={"3"}>
-          <form action={userLogin}>
-            <Flex direction={"column"} gap={"1"}>
+    <Container height={"100dvh"}>
+      <Flex
+        direction={"column"}
+        align={"center"}
+        justify={"center"}
+        height={"100%"}
+      >
+        <Card size={"3"}>
+          <Flex
+            asChild
+            direction={"column"}
+            gap={"2"}
+            width={"calc(var(--space-5)*9)"}
+          >
+            <form action={userLogin}>
+              <Flex justify={"center"} mb={"3"}>
+                <Avatar size={"9"} radius="full" fallback />
+              </Flex>
               <TextField.Root type="email" placeholder="Email" name="email" />
               <TextField.Root
                 type="password"
                 placeholder="Password"
                 name="password"
               />
-              <Button type="submit">Login</Button>
-            </Flex>
-          </form>
+              <Flex direction={"column"} mt={"2"}>
+                <Button type="submit">Login</Button>
+              </Flex>
+            </form>
+          </Flex>
         </Card>
-      </Container>
-    </Flex>
+      </Flex>
+    </Container>
   );
 }
