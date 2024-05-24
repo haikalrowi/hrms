f08b4c9ed67da546847be984722c1682f5ca757e
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-24 08:38:55 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { Dashboard } from "@/components/Dashboard";
 import { Login } from "@/components/Login";
 import { userContext } from "@/lib/action";
-import { cookies } from "next/headers";
 
-export default async function Home() {
-  const userId = cookies().get("userLogin")?.value;
-  if (userId) {
-    const currentContext = await userContext(userId);
+export default async function User() {
+  try {
+    const currentContext = await userContext();
     return <Dashboard {...currentContext} />;
+  } catch (error) {
+    return <Login />;
   }
-  return <Login />;
 }
