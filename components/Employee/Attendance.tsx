afa4f2c9a5ee0bca1f6d57df5c48e1defea24c57
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-25 20:32:06 +0700

    Update Attendance.tsx

--- components/Employee/Attendance.tsx
+++ components/Employee/Attendance.tsx
@@ -1,8 +1,54 @@
 import { UserContext } from "@/context/Dashboard";
 import { employeeUpdateAttendance } from "@/lib/action";
 import { getAttendanceStatus } from "@/lib/utils";
+import { Attendance } from "@prisma/client";
 import { Badge, Button, Container, Table } from "@radix-ui/themes";
 import { useContext, useState } from "react";
+
+function AttendanceRow(attendance: Attendance) {
+  const [checkInPending, setCheckInPending] = useState(false);
+  const [checkOutPending, setCheckOutPending] = useState(false);
+  const checkIn = async () => {
+    setCheckInPending(true);
+    await employeeUpdateAttendance(attendance.id, "checkInAt");
+    setCheckInPending(false);
+  };
+  const checkOut = async () => {
+    setCheckOutPending(true);
+    await employeeUpdateAttendance(attendance.id, "checkOutAt");
+    setCheckOutPending(true);
+  };
+  const attendanceStatus = getAttendanceStatus(attendance);
+  return (
+    <Table.Row>
+      <Table.Cell>{attendance.checkInDate.toLocaleString()}</Table.Cell>
+      <Table.Cell>{attendance.checkOutDate.toLocaleString()}</Table.Cell>
+      <Table.Cell>
+        {attendanceStatus.checkInStatus === "No status" ? (
+          <Button variant="ghost" loading={checkInPending} onClick={checkIn}>
+            Check in
+          </Button>
+        ) : (
+          <Badge>{attendanceStatus.checkInStatus}</Badge>
+        )}
+      </Table.Cell>
+      <Table.Cell>
+        {attendanceStatus.checkOutStatus &&
+          (attendanceStatus.checkOutStatus === "No status" ? (
+            <Button
+              variant="ghost"
+              loading={checkOutPending}
+              onClick={checkOut}
+            >
+              Check out
+            </Button>
+          ) : (
+            <Badge>{attendanceStatus.checkOutStatus}</Badge>
+          ))}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
 
 export function EmployeeAttendance() {
   const userContext = useContext(UserContext);
@@ -19,58 +65,9 @@ export function EmployeeAttendance() {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {userContext.user?.Employee?.Attendance.map((attendance) => {
-            const [checkInPending, setCheckInPending] = useState(false);
-            const [checkOutPending, setCheckOutPending] = useState(false);
-            const checkIn = async () => {
-              setCheckInPending(true);
-              await employeeUpdateAttendance(attendance.id, "checkInAt");
-              setCheckInPending(false);
-            };
-            const checkOut = async () => {
-              setCheckOutPending(true);
-              await employeeUpdateAttendance(attendance.id, "checkOutAt");
-              setCheckOutPending(true);
-            };
-            const attendanceStatus = getAttendanceStatus(attendance);
-            return (
-              <Table.Row key={attendance.id}>
-                <Table.Cell>
-                  {attendance.checkInDate.toLocaleString()}
-                </Table.Cell>
-                <Table.Cell>
-                  {attendance.checkOutDate.toLocaleString()}
-                </Table.Cell>
-                <Table.Cell>
-                  {attendanceStatus.checkInStatus === "No status" ? (
-                    <Button
-                      variant="ghost"
-                      loading={checkInPending}
-                      onClick={checkIn}
-                    >
-                      Check in
-                    </Button>
-                  ) : (
-                    <Badge>{attendanceStatus.checkInStatus}</Badge>
-                  )}
-                </Table.Cell>
-                <Table.Cell>
-                  {attendanceStatus.checkOutStatus &&
-                    (attendanceStatus.checkOutStatus === "No status" ? (
-                      <Button
-                        variant="ghost"
-                        loading={checkOutPending}
-                        onClick={checkOut}
-                      >
-                        Check out
-                      </Button>
-                    ) : (
-                      <Badge>{attendanceStatus.checkOutStatus}</Badge>
-                    ))}
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
+          {userContext.user?.Employee?.Attendance.map((attendance) => (
+            <AttendanceRow key={attendance.id} {...attendance} />
+          ))}
         </Table.Body>
       </Table.Root>
     </Container>
